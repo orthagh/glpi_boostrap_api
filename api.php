0b7b6ab081301580061d372b9d7e75b64f4b652d
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2017-03-21 09:50:25 +0100

    remove orphan include

--- api.php
+++ api.php
@@ -1,7 +1,5 @@
 <?php
 
-include_once __DIR__."/index.php";
-
 if (!file_exists(__DIR__.'/vendor/autoload.php')) {
    throw new Exception("No vendor found, please run composer install --no-dev");
 }
